ec94f697e5f09da5bb308f28766abb3671637ec2
suyashverma0 <boyfact560@gmail.com>
2026-07-30 05:52:44 +0530

    basic done

---
+++ oops1.py
@@ -0,0 +1,17 @@
+class Employee :
+    def __init__(self):
+        self.name="ram"
+        self.salary=50000
+        self.designation="ml engineer"
+
+    def travel(self,destination):
+        print(f"employee is travling to this destination{destination}")
+
+
+
+
+
+
+e1 = Employee()
+print(e1.name)
+e1.travel("ramswaroop") 
